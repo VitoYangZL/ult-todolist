2748b3b0d8bdd3e1335c3eee8a45fb083572bf61
VitoYang <yutcpp41100202@gmail.com>
2022-03-10 20:24:07 +0800

    after dinner

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
-const formElements = [...document.querySelector(".input-container").children];
-const [todoText, todoCategory, todoMonth, todoDate, submitt] = [
-  ...formElements.value,
-];
+const submittBtn = document.querySelector(".input-submitt")
 const todoArea = document.querySelector(".todo-area");
 
-submitt.addEventListener("click", (e) => {
+submittBtn.addEventListener("click", (e) => {
   e.preventDefault();
+  let form = e.target.parentElement;
+  console.log(form);
+  let todoText = form[0].value;
+  let todoCategory = form[1].value;
+  let todoMonth = form[2].value;
+  let todoDate = form[3].value;
+  if (todoText === "" || todoCategory ==="") {
+    alert("You forgot to write something.");
+    return;
+  }
+  if (todoMonth > 12 || todoMonth < 1 || todoMonth === "") {
+    alert("Your Month is weired , plz try again");
+    return;
+  }
+  if (todoDate > 31 || todoDate < 1 || todoDate === "") {
+    alert("Your Date is weired , plz try again");
+    return;
+  }
+  if (todoMonth === "2") {
+    if (todoDate > 29 || todoDate < 1 || todoDate === "") {
+      alert("What's wrong with your February?");
+      return;
+    }
+  }
 
-  if (!todoText) return alert("You forgot to write something!");
-  if (todoMonth > 12 || todoMonth < 1 || todoMonth == "")
-    return alert("Weired Month Value!");
-  if (todoDate > 31 || todoMonth < 1 || todoDate == "")
-    return alert("Weired Date Value!");
-  let todoTime = `${todoMonth}/${todoDate}`;
+  let todo = document.createElement("div");
+  todo.setAttribute("draggable", true);
+  todo.classList.add("todo-container");
+  let text = document.createElement("p");
+  text.classList.add("todo-content");
+  text.innerText = todoText;
+  let category = document.createElement("p");
+  category.classList.add("todo-category");
+  category.innerText = todoCategory;
+  let time = document.createElement("p");
+  time.classList.add("todo-time");
+  time.innerText = `${todoMonth}/${todoDate}`;
+  todoArea.appendChild(todo);
+  todo.appendChild(text);
+  todo.appendChild(category);
+  todo.appendChild(time);
 
-  let newDiv = document.createElement("div");
-  newDiv.classList.add("todo-container");
-  let newText = document.createElement("p");
-  newText.classList.add("todo-content");
-  newText = todoText;
-  let newCategory = document.createElement("p");
-  newCategory.classList.add("todo-category");
-  newCategory = todoCategory;
-  let newTime = document.createElement("p");
-  newTime.classList.add("todo-time");
-  newTime = todoTime;
+  //check button
+  let checkBtn = document.createElement('i')
+  checkBtn.innerHTML = `<i class="fa fa-check check-button"></i>`;
+  todo.appendChild(checkBtn);
 
-  newDiv.appendChild(newText);
-  newDiv.appendChild(newCategory);
-  newDiv.appendChild(newTime);
+  checkBtn.addEventListener('click', e=>{
+
+    let todoItem = e.target.parentElement.parentElement;
+    todoItem.classList.toggle('check-toggle')
+  })
+
+
+
 });
