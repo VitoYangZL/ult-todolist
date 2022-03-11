c1748e7f16ab2732fc3015f55dea40956ff33f12
Vito Yang <yutcpp41100202@gmail.com>
2022-03-11 17:29:49 +0800

    local storage

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-const submittBtn = document.querySelector(".input-submitt")
+const submittBtn = document.querySelector(".input-submitt");
 const todoArea = document.querySelector(".todo-area");
+
+loadData();
 
 submittBtn.addEventListener("click", (e) => {
   e.preventDefault();
   let form = e.target.parentElement;
-  console.log(form);
   let todoText = form[0].value;
   let todoCategory = form[1].value;
   let todoMonth = form[2].value;
   let todoDate = form[3].value;
-  if (todoText === "" || todoCategory ==="") {
+  if (todoText === "" || todoCategory === "") {
     alert("You forgot to write something.");
     return;
   }
@@ -40,22 +41,107 @@ submittBtn.addEventListener("click", (e) => {
   let time = document.createElement("p");
   time.classList.add("todo-time");
   time.innerText = `${todoMonth}/${todoDate}`;
-  todoArea.appendChild(todo);
+
   todo.appendChild(text);
   todo.appendChild(category);
   todo.appendChild(time);
-
   //check button
-  let checkBtn = document.createElement('i')
+  let checkBtn = document.createElement("i");
   checkBtn.innerHTML = `<i class="fa fa-check check-button"></i>`;
-  todo.appendChild(checkBtn);
-
-  checkBtn.addEventListener('click', e=>{
-
+  checkBtn.addEventListener("click", () => {
     let todoItem = e.target.parentElement.parentElement;
-    todoItem.classList.toggle('check-toggle')
-  })
+    todoItem.classList.toggle("check-toggle");
+  });
 
+  let removeBtn = document.createElement("i");
+  removeBtn.innerHTML = `<i class="fas fa-trash remove-button"></i>`;
+  removeBtn.addEventListener("click", (e) => {
+    let removeItem = e.target.parentElement.parentElement;
+    let keyText = removeItem.childNodes[0].innerText;
+    let listArray = JSON.parse(localStorage.getItem("list"));
+    listArray.forEach((item, index) => {
+      if (item.todoText == keyText) {
+        listArray.splice(index, 1);
+        localStorage.setItem("list", JSON.stringify(listArray));
+      }
+      removeItem.remove();
+      console.log("remove successfuly");
+    });
+  });
 
+  todo.appendChild(checkBtn);
+  todo.appendChild(removeBtn);
 
+  let todoObject = {
+    todoText: todoText,
+    todoCategory: todoCategory,
+    todoMonth: todoMonth,
+    todoDate: todoDate,
+  };
+
+  let myList = localStorage.getItem("list");
+  if (myList == null) {
+    localStorage.setItem("list", JSON.stringify([todoObject]));
+  } else {
+    let newListArray = JSON.parse(myList);
+    newListArray.push(todoObject);
+    localStorage.setItem("list", JSON.stringify(newListArray));
+  }
+  form.children[0].value = "";
+  form.children[1].value = "";
+  form.children[2].value = "";
+  form.children[3].value = "";
+  todoArea.appendChild(todo);
 });
+
+function loadData() {
+  let myList = localStorage.getItem("list");
+  if (myList !== null) {
+    let listArray = JSON.parse(myList);
+    listArray.forEach((item) => {
+      let todo = document.createElement("div");
+      todo.setAttribute("draggable", true);
+      todo.classList.add("todo-container");
+      let text = document.createElement("p");
+      text.classList.add("todo-content");
+      text.innerText = item.todoText;
+      let category = document.createElement("p");
+      category.innerText = item.todoCategory;
+      category.classList.add("todo-category");
+      let time = document.createElement("p");
+      time.classList.add("todo-time");
+      time.innerText = `${item.todoMonth}/${item.todoDate}`;
+
+      todo.appendChild(text);
+      todo.appendChild(category);
+      todo.appendChild(time);
+
+      let checkBtn = document.createElement("i");
+      checkBtn.innerHTML = `<i class="fa fa-check check-button"></i>`;
+      checkBtn.addEventListener("click", (e) => {
+        let todoItem = e.target.parentElement.parentElement;
+        todoItem.classList.toggle("check-toggle");
+      });
+
+      let removeBtn = document.createElement("i");
+      removeBtn.innerHTML = `<i class="fas fa-trash remove-button"></i>`;
+      removeBtn.addEventListener("click", (e) => {
+        let removeItem = e.target.parentElement.parentElement;
+        let keyText = removeItem.childNodes[0].innerText;
+        let listArray = JSON.parse(localStorage.getItem("list"));
+        listArray.forEach((item, index) => {
+          if (item.todoText == keyText) {
+            listArray.splice(index, 1);
+            localStorage.setItem("list", JSON.stringify(listArray));
+          }
+          removeItem.remove();
+          console.log("remove successfuly");
+        });
+      });
+
+      todo.appendChild(checkBtn);
+      todo.appendChild(removeBtn);
+      todoArea.appendChild(todo);
+    });
+  }
+}
